11f9c79336a868c3875ee79a6ae720134cebda00
Jacob Godwin <jpg174@gmail.com>
2022-07-26 18:16:19 -0400

    add image and css handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hey buddy!');
+import './style.css';
+import Logo from './los-pollos-logo.png';
+
+function component() {
+  const element = document.getElementById('content');
+
+  element.innerHTML = `What's up!`;
+
+  // Add the image to our existing div.
+
+  const myLogo = new Image();
+
+  myLogo.src = Logo;
+
+  element.appendChild(myLogo);
+
+  return element;
+}
+
+document.body.appendChild(component());
