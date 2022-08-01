e16e00bc9fa15a38c23409416f94e99d795f456d
Jacob Godwin <jpg174@gmail.com>
2022-08-01 07:02:01 -0400

    initial html layout

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import './style.css';
-import Logo from './los-pollos-logo.png';
+// import Logo from './los-pollos-logo.png';
 
-function component() {
-  const element = document.getElementById('content');
+// function component() {
+//   const element = document.getElementById('content');
 
-  element.innerHTML = `What's up!`;
+//   element.innerHTML = `What's up!`;
 
-  // Add the image to our existing div.
+//   // Add the image to our existing div.
 
-  const myLogo = new Image();
+//   const myLogo = new Image();
 
-  myLogo.src = Logo;
+//   myLogo.src = Logo;
 
-  element.appendChild(myLogo);
+//   element.appendChild(myLogo);
 
-  return element;
-}
+//   return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
